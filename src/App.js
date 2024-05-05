7b637ff65f430316d6837b3bdb60ec573e51adf7
Abhay <26abhaykumarsingh@gmail.com>
2024-05-06 04:34:58 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Filters from "./components/Filters/Filters";
 import JobsList from "./components/Jobs/JobsList";
 import { useDispatch } from "react-redux";
 import { fetchJobsAsync } from "./components/Jobs/jobsSlice";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Filters />
       <JobsList></JobsList>
     </div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="weekdayLogo"></div>
+      <div className="blackBox">Assignment submission</div>
+    </div>
+  );
+}
+
+export default Header;
