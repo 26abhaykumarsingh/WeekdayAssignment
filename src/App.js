217f88d1b07f340e66a244c7e79080005d8996a4
Abhay <26abhaykumarsingh@gmail.com>
2024-05-06 15:50:52 +0530

    Remove filters during initial loader

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { useEffect } from "react";
 import "./App.css";
 import Filters from "./components/Filters/Filters";
 import JobsList from "./components/Jobs/JobsList";
-import { useDispatch } from "react-redux";
-import { fetchJobsAsync } from "./components/Jobs/jobsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchJobsAsync,
+  selectFilteredJobs,
+  selectStatus,
+} from "./components/Jobs/jobsSlice";
 import Header from "./components/Header/Header";
 
 function App() {
+  const status = useSelector(selectStatus);
+  const filteredJobs = useSelector(selectFilteredJobs);
   return (
     <div className="App">
       <Header />
-      <Filters />
+      {!(status === "loading" && filteredJobs.length === 0) && <Filters />}
       <JobsList></JobsList>
     </div>
   );
